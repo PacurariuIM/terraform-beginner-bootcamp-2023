40f5662e9e932fa6232004bb1d3836ce28fbf116
PacurariuIM <jonny_boy90609@yahoo.com>
2023-10-12 18:54:11 +0000

    last fixes

--- main.tf
+++ main.tf
@@ -5,14 +5,6 @@ terraform {
       version = "1.0.0"
     }
   }
-  #backend "remote" {
-  #  hostname = "app.terraform.io"
-  #  organization = "ExamPro"
-
-  #  workspaces {
-  #    name = "terra-house-1"
-  #  }
-  #}
   cloud {
    organization = "crazyfroggg"
    workspaces {
@@ -36,16 +28,16 @@ module "home_medianxl_hosting" {
 }
 
 resource "terratowns_home" "home" {
-  name = "Median XL Sigma- a Diablo 2 Mod"
+  name = "Don't Starve Together- A survival game"
   description = <<DESCRIPTION
-The most popular Diablo II overhaul modification, 
-Median XL is an action RPG with extensive endgame content, 
-deep character customisation and challenging gameplay. 
-It offers thousands of new items, new skills for all classes, 
-and multiple improvements to the Diablo II engine.
+Don't Starve Together is the standalone multiplayer expansion 
+of the uncompromising wilderness survival game, Don't Starve. 
+Enter a strange and unexplored world full of strange creatures, 
+dangers, and surprises. Gather resources to craft items and 
+structures that match your survival style.
 DESCRIPTION
   domain_name = module.home_medianxl_hosting.domain_name
-  town = "missingo"
+  town = "gamers-grotto"
   content_version = var.medianxl.content_version
 }
 
@@ -59,11 +51,7 @@ module "home_starve_hosting" {
 resource "terratowns_home" "home_starve" {
   name = "Don't Starve Together- Survival co-op adventure"
   description = <<DESCRIPTION
-Don't Starve Together is the standalone multiplayer expansion 
-of the uncompromising wilderness survival game, Don't Starve. 
-Enter a strange and unexplored world full of strange creatures, 
-dangers, and surprises. Gather resources to craft items and 
-structures that match your survival style.
+None
 DESCRIPTION
   domain_name = module.home_starve_hosting.domain_name
   town = "missingo"
